3382e757ca4c4f1e0cd54b58a19f9d6b83ab5e92
Павел <apaex@ngs.ru>
2022-07-27 11:09:11 +0300

    ESPHome WIFI

---
+++ aquarium.h
@@ -0,0 +1,56 @@
+#pragma once
+
+namespace aquarium {
+
+//static const uint8_t AQUARIUM_COMMAND_GET_PPM[] = {0xFF, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00};
+static const uint8_t AQUARIUM_COMMAND_GET_PPM[] = {0xFF};
+static const uint8_t AQUARIUM_REQUEST_LENGTH = sizeof(AQUARIUM_COMMAND_GET_PPM);
+static const uint16_t AQUARIUM_PREAMBLE = 0x86FF;
+
+uint8_t AQUARIUM_checksum(const uint8_t *command, uint8_t count);
+
+typedef struct __attribute__((__packed__)) 
+{
+	uint16_t sign;
+	uint8_t pH;
+	uint8_t light;
+	uint8_t level;
+	uint8_t temperature;
+	uint8_t crc;
+
+private:
+    uint8_t pack(float v, float scale, float offs)    {        return uint8_t((v - offs) * scale) ;    }
+    float unpack(uint8_t v, float scale, float offs)  {        return float(v) / scale + offs;        }
+
+public:
+    void setPH(float v)             { pH = pack(v, 10, 0);    }
+    void setLight(float v)          { light = pack(v, 0.5, 0);    }
+    void setLevel(float v)          { level = pack(v, 2, 0);    }
+    void setTemperature(float v)    { temperature = pack(v, 10, 10);    }
+
+    float getPH()             {  return unpack(pH, 10, 0);    }
+    float getLight()          {  return unpack(light, 0.5, 0);    }
+    float getLevel()          {  return unpack(level, 2, 0);    }
+    float getTemperature()    {  return unpack(temperature, 10, 10);    }
+
+    void setSign() { sign = AQUARIUM_PREAMBLE; }
+    bool checkSign() { return sign == AQUARIUM_PREAMBLE; }
+    void setCRC() { setSign(); crc = AQUARIUM_checksum((uint8_t *)this, sizeof(this)-1); }
+    bool checkCRC() { return crc == AQUARIUM_checksum((uint8_t *)this, sizeof(this)-1); }
+
+} DataContainer;
+
+static const uint8_t AQUARIUM_RESPONSE_LENGTH = sizeof(DataContainer);
+
+uint8_t AQUARIUM_checksum(const uint8_t *command, uint8_t count) 
+{
+    uint8_t sum = 0;
+    for (uint8_t i = 1; i < count; i++) 
+    {
+        sum += command[i];
+    }
+    return 0xFF - sum + 0x01;
+}
+
+
+}
---
+++ aquarium_component.h
@@ -0,0 +1,107 @@
+#pragma once
+
+#include "esphome.h"
+
+using namespace aquarium;
+
+namespace esphome
+{
+	namespace aquarium
+	{
+
+		static const char *const TAG = "aquarium";
+
+		class AquariumComponent : public PollingComponent, public UARTDevice
+		{
+		private:
+			int incomingByte = 0;
+
+		public:
+			Sensor *pH_sensor = new Sensor();
+			Sensor *light_sensor = new Sensor();
+			Sensor *level_sensor = new Sensor();
+			Sensor *temperature_sensor = new Sensor();
+
+			AquariumComponent(UARTComponent *parent) : PollingComponent(5000), UARTDevice(parent) {}
+
+			float get_setup_priority() const override { return setup_priority::DATA; }
+
+			void setup() override
+			{
+			}
+
+			void update() override
+			{
+				DataContainer response;
+				if (!this->AQUARIUM_write_command_(AQUARIUM_COMMAND_GET_PPM, (uint8_t *)&response))
+				{
+					ESP_LOGW(TAG, "Reading data from AQUARIUM failed!");
+					this->status_set_warning();
+					return;
+				}
+/*
+				char buff[80];
+				for (int i = 0; i < AQUARIUM_RESPONSE_LENGTH; ++i) 
+				{
+					uint8_t d = ((uint8_t *)&response)[i];
+					sprintf(buff + i * 3, "%02X  ", d);
+				}
+				ESP_LOGW(TAG, "%s", buff);
+*/
+				if (!response.checkSign())
+				{
+					ESP_LOGW(TAG, "Invalid preamble from AQUARIUM!");
+					this->status_set_warning();
+					return;
+				}
+
+				if (!response.checkCRC())
+				{
+					ESP_LOGW(TAG, "AQUARIUM Checksum doesn't match");
+					this->status_set_warning();
+					return;
+				}
+
+				this->status_clear_warning();
+
+				float pH = response.getPH();
+				float light = response.getLight();
+				float level = response.getLevel();
+				float temperature = response.getTemperature();
+
+//				ESP_LOGD(TAG, "AquariumComponent Received pH?=%f light=%flx level=%f%% temp=%f°C", pH, light, level, temperature);
+
+				pH_sensor->publish_state(pH);
+				light_sensor->publish_state(light);
+				level_sensor->publish_state(level);
+				temperature_sensor->publish_state(temperature);
+			}
+
+			void dump_config() override
+			{
+				return PollingComponent::dump_config();
+			}
+
+			void loop() override
+			{
+			}
+
+		protected:
+			bool AQUARIUM_write_command_(const uint8_t *command, uint8_t *response)
+			{
+				// Empty RX Buffer
+				while (this->available())
+					this->read();
+				this->write_array(command, AQUARIUM_REQUEST_LENGTH);
+				this->write_byte(AQUARIUM_checksum(command, AQUARIUM_REQUEST_LENGTH));
+				this->flush();
+
+				if (response == nullptr)
+					return true;
+
+				return this->read_array(response, AQUARIUM_RESPONSE_LENGTH);
+			}
+		};
+
+	} // namespace aquarium
+} // namespace esphome
